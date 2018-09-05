a05673784ac08b3dce5e5a03d30e7c546e71846b
Vahram Martinyan <vahram.martinyan@gmail.com>
2018-09-05 16:56:06 +0300

    Home page modification

---
+++ wp-content/uploads/custom-css-js/948.js
@@ -0,0 +1,15 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+/* Default comment here */ 
+
+jQuery(document).ready(function($){
+
+  	//Height of post grid hover item
+    //jQuery(".postgridItem ").height(jQuery(".postgridItem ").closest(".vc_gitem-zone").height())
+
+  alert(jQuery("div.vc_grid-item").height());
+    $("div.vc_row").height(360);
+});
+
+</script>
+<!-- end Simple Custom CSS and JS -->
